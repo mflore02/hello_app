29df35d2f8b94bd1a39e67f5ded5143406c35664
“Martha” <martha.dfwrealtor@gmail.com>
2018-10-02 10:35:57 -0500

    favorite restaurants

---
+++ blackbear.js
@@ -0,0 +1,16 @@
+// function heightincentemeters() {
+
+// 	var x = prompt("What is your height")
+// 	var heightincentemeters = 2.54 * x
+// 	console.log(heightincentemeters)
+// }
+
+// heightincentemeters()
+// x = prompt("is this a dog or cat?")
+
+
+// if (x === "dog") {
+// 	console.log("woof")
+// } else {
+// 	console.log("meow")
+// }
